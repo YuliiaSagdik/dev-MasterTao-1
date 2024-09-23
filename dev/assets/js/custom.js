02d9869f4cb957d1fdb2bac0c0b03c0aa20a61f8
YuliiaSagdik <sagdik.86@gmail.com>
2024-09-23 11:55:45 +0300

    feat:banner done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,22 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    speed: 2000,
+    avtoplay: {
+        delay:3000,
+    },
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+   
+  }); 
