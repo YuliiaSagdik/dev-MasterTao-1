d466ca1375d276aed30126d323e68f3c27d83d78
YuliiaSagdik <sagdik.86@gmail.com>
2024-10-14 10:51:45 +0300

    feat:creat delivery layout

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,27 +1,30 @@
-const swiper = new Swiper('#banner-slider', {
-    loop: true,
-    speed: 2000,
-    avtoplay: {
-        delay:3000,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      enabled: false,
-    },
-    breakpoints: {
-      // when window width is >= 992px
-      992: {
-        navigation: {
-          enabled: true,
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
+const swiperBanner = new Swiper('#banner-slider', {
+  loop: true,
+  spaceBetween: 12,
+  speed: 2000,
+  avtoplay: {
+      delay:3000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    enabled: false,
+  },
+  breakpoints: {
+    // when window width is >= 992px
+    992: {
+      navigation: {
+        enabled: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
       },
-    }
-  }); 
+    },
+  }
+}); 
+
+
   const typeFile = document.querySelectorAll('.custom-file')
   typeFile.forEach(item =>{
     item.addEventListener('change', () => {
@@ -29,7 +32,7 @@ const swiper = new Swiper('#banner-slider', {
       fileName.innerHTML = item.files[0].name;
 
     } )
-   } )
+   } );
    const advantagesSlideLength = document.querySelectorAll('#advantages .swiper-slide')
    const swiperAdvantages = new Swiper('#advantages',{ 
    loop: true, 
@@ -50,7 +53,59 @@ const swiper = new Swiper('#banner-slider', {
         enabled: false, 
         el: '.swiper-pagination',
        
-       }
-    }
+       },
+    },
 }
+});
+
+const swiperSliderTemplate = new Swiper('.slider-template', {
+  speed: 400,
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true, 
+      enabled: true,   
+  },
+  navigation: {
+      enabled: false,
+      nextEl: '.swiper-button-next-unique',
+      prevEl: '.swiper-button-prev-unique'
+  },
+
+  breakpoints: {
+     767: {
+      slidesPerView: 1,
+      navigation: {
+          enabled: true,
+          nextEl: '.swiper-button-next-unique',
+          prevEl: '.swiper-button-prev-unique'
+      },
+     },
+      861: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+          navigation: {
+              enabled: true,
+              nextEl: '.swiper-button-next-unique',
+              prevEl: '.swiper-button-prev-unique'
+          },
+          pagination: {
+            enabled: false,
+        },
+      },
+
+      1252: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          navigation: {
+              enabled: true,
+              nextEl: '.swiper-button-next-unique',
+              prevEl: '.swiper-button-prev-unique'
+          },
+          pagination: {
+            enabled: false,
+        },
+      },
+  },
 });
